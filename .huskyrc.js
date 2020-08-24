56e84cbe022905d184f292bccdcc4b8ec6f52e0d
SSen <i@ssen.name>
2020-08-24 13:26:58 +0900

    2020.08.24

---
+++ .huskyrc.js
@@ -0,0 +1,13 @@
+function precommit(...workspaces) {
+  return workspaces.map((workspace) => `yarn workspace ${workspace} run precommit`);
+}
+
+module.exports = {
+  hooks: {
+    'pre-commit': [
+      `markdown-source-import "{,!(node_modules)/**/}*.md" --git-add`,
+      `lint-staged`,
+      //...precommit(`source`),
+    ].join(' && '),
+  },
+};
